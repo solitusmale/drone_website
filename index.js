9180e343a096cdaf4b29af2e3f7fd1e32846443b
Aleksa <alexaw32@hotmail.com>
2025-03-03 16:29:38 +0100

    JS

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 // Select container
 const container = document.getElementById('drone-container');
 
+const navSearch =document.getElementById('nav-search')
+const searchIcon =document.getElementById('search-icon')
+
 // Setup Scene, Camera, Renderer
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, container.clientWidth / container.clientHeight, 0.1, 1000);
@@ -63,3 +66,108 @@ window.addEventListener('resize', () => {
 
 // Start Animation
 animate();
+
+searchIcon.addEventListener('click', (e)=>{
+    e.stopPropagation();
+    navSearch.classList.toggle('active')
+});
+
+document.addEventListener('click',(e)=>{
+    if(!navSearch.contains(e.target)){
+        navSearch.classList.remove('active')
+    }
+})
+
+//navbar scroll
+window.addEventListener('scroll',()=>{
+    let navbar=document.querySelector('.navbar');
+    if(window.scrollY >20){
+        navbar.classList.add('scrolled')
+    }else{
+        navbar.classList.remove('scrolled')
+    }
+});
+
+//Scroll_top
+
+document.querySelector('#to-top').addEventListener('click',()=>{
+
+    let TopInterval = setInterval(()=>{
+        
+        let ArrowTop = document.body.scrollTop > 0 ? document.body : document.documentElement;
+
+        if(ArrowTop.scrollTop > 0){
+            ArrowTop.scrollTop = ArrowTop.scrollTop - 50 ;
+        }
+        if(ArrowTop.scrollTop < 1){
+            clearInterval(TopInterval);
+        }
+    }, 10)
+}, false);
+
+function showscroll(){
+    let TopButton = document.getElementById('to-top');
+    if(document.body.scrollTop > 100 || document.documentElement.scrollTop >100){
+        TopButton.classList.add('show')
+    }else{
+        TopButton.classList.remove('show')
+    }
+}
+
+window.onscroll = () =>{
+    showscroll();
+} 
+
+// sidebar
+let burger =document.querySelector('.burger')
+let NavbarItems =document.querySelector('.navbar-items')
+let nav =document.querySelector('.nav');
+
+burger.addEventListener('click',()=>{
+    NavbarItems.classList.toggle('v-class')
+    nav.classList.toggle('h-class')
+});
+
+// scrollRevealAnimation
+
+ScrollReveal().reveal('.icons i',{
+    duration:1000,
+    origin:'bottom',
+    distance:'50px',
+    interval:200,
+    reset:true
+});
+ScrollReveal().reveal('.A_img img ',{
+    duration:1000,
+    origin:'bottom',
+    distance:'100px',
+    interval:200,
+    reset:true
+});
+ScrollReveal().reveal('.About_box',{
+    duration:1000,
+    origin:'bottom',
+    distance:'100px',
+    interval:200,
+    reset:true
+});
+ScrollReveal().reveal('.b_box',{
+    duration:1000,
+    origin:'bottom',
+    distance:'100px',
+    interval:200,
+    reset:true
+});
+ScrollReveal().reveal('.box', {
+    duration: 1000,
+    origin: 'bottom',
+    distance: '100px',
+    interval: 200
+  });
+ScrollReveal().reveal('.col',{
+    duration:1000,
+    origin:'bottom',
+    distance:'100px',
+    interval:200,
+    reset:true
+})
